c672a490963986808d5c29fd9a2cc9f3a294b5a1
Adam Tyson <code@adamltyson.com>
2024-09-17 16:58:20 +0100

    Simplify examples

--- demo/array_example.sh
+++ demo/array_example.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -p gpu # partition (queue)
+#SBATCH -p fast # partition (queue)
 #SBATCH -N 1   # number of nodes
-#SBATCH --mem 2G # memory pool for all cores
-#SBATCH -n 2 # number of cores
-#SBATCH -t 0-0:10 # time (D-HH:MM)
+#SBATCH --mem 1G # memory pool for all cores
+#SBATCH -n 1 # number of cores
+#SBATCH -t 0-0:1 # time (D-HH:MM)
 #SBATCH -o slurm_array_%A-%a.out
 #SBATCH -e slurm_array_%A-%a.err
 #SBATCH --array=0-9%4
@@ -12,9 +12,6 @@
 # Array job runs 10 separate jobs, but not more than four at a time.
 # This is flexible and the array ID ($SLURM_ARRAY_TASK_ID) can be used in any way.
 
-module load miniconda
-conda activate slurm_demo
-
 echo "Multiplying $SLURM_ARRAY_TASK_ID by 10"
-python multiply.py $SLURM_ARRAY_TASK_ID 10 --jazzy
+./multiply.sh $SLURM_ARRAY_TASK_ID 10 
 
---
+++ demo/multiply.sh
@@ -0,0 +1,3 @@
+result=$(($1 * $2))
+
+echo "The result of multiplying $1 and $2 is: $result"
